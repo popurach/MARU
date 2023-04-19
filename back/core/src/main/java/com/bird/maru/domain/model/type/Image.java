cf6c63354e4dd9ecf13ad9d54b4277097e4c0574
DaWoon Lee <qkgk61@gmail.com>
2023-04-19 16:55:45 +0900

    :sparkles: Feat: 멤버 기본 프로필 이미지를 위한 정적 팩토리 메서드 정의

--- back/core/src/main/java/com/bird/maru/domain/model/type/Image.java
+++ back/core/src/main/java/com/bird/maru/domain/model/type/Image.java
@@ -1,7 +1,12 @@
 package com.bird.maru.domain.model.type;
 
+import com.bird.maru.domain.converter.UrlConverter;
+import java.io.UncheckedIOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Converter;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -27,6 +32,18 @@ public class Image {
 
     @Column(name = "image_url")
     @NotNull
+    @Convert(converter = UrlConverter.class)
     private URL imageUrl;
+
+    public static Image getDefaultMemberProfile() {
+        try {
+            return Image.builder()
+                        .savedPath("images/members/default_profile.png")
+                        .imageUrl(new URL("https://maruofbucket.s3.ap-northeast-2.amazonaws.com/images/members/default_profile.png"))
+                        .build();
+        } catch (MalformedURLException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 
 }
