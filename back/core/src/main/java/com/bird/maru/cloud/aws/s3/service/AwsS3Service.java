1577dafc5754573701983db4ca6c53e774b846a4
DaWoon Lee <qkgk61@gmail.com>
2023-04-19 15:50:51 +0900

    :sparkles: Feat: S3에 이미지 오브젝트 업로드 구현

--- back/core/src/main/java/com/bird/maru/cloud/aws/s3/service/AwsS3Service.java
+++ back/core/src/main/java/com/bird/maru/cloud/aws/s3/service/AwsS3Service.java
@@ -1,8 +1,16 @@
 package com.bird.maru.cloud.aws.s3.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.bird.maru.domain.model.type.Image;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class AwsS3Service {
@@ -14,5 +22,46 @@ public class AwsS3Service {
         this.amazonS3 = amazonS3;
         this.bucketName = bucketName;
     }
+
+    /**
+     * 멤버의 프로필 사진을 저장합니다.
+     *
+     * @param image 저장할 1개의 프로필 사진
+     * @return Image 객체를 반환합니다. 이 객체는 {@code S3Object}에 대한 기본 정보(이름, 경로, URL)를 담고 있습니다.
+     */
+    public Image uploadMemberProfile(MultipartFile image) {
+        return upload(image, "images/members");
+    }
+
+    private Image upload(MultipartFile image, String path) {
+        String key = createKey(path);
+
+        try (InputStream input = image.getInputStream()) {
+            amazonS3.putObject(
+                    new PutObjectRequest(
+                            this.bucketName, key, input, getObjectMetadata(image)
+                    ).withCannedAcl(CannedAccessControlList.PublicRead)
+            );
+        } catch (IOException e) {
+            throw new IllegalArgumentException("업로드 하려는 파일을 읽을 수 없습니다.", e);
+        }
+
+        return Image.builder()
+                    .savedPath(key)
+                    .imageUrl(amazonS3.getUrl(this.bucketName, key))
+                    .build();
+    }
+
+    private String createKey(String path) {
+        return path + "/" + UUID.randomUUID();
+    }
+
+    private ObjectMetadata getObjectMetadata(MultipartFile multipartFile) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(multipartFile.getContentType());
+        metadata.setContentLength(multipartFile.getSize());
+        return metadata;
+    }
+
 
 }
