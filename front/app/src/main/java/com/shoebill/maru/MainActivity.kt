74e0460bd4074afe61ffe919b00813b1af3a28c4
ludwings0330 <ludwings0330@gmail.com>
2023-04-20 22:31:37 +0900

    :sparkles: Feat: Push 알림 구현
    - FCM 에서 Notification 전송시 Push 알림
    - 포르가운드일때 어떻게 해야할지 추가 구현 필요

--- front/app/src/main/java/com/shoebill/maru/MainActivity.kt
+++ front/app/src/main/java/com/shoebill/maru/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.shoebill.maru
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +16,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.shoebill.maru.ui.page.MainPage
 import com.shoebill.maru.ui.page.NoticePage
 import com.shoebill.maru.ui.theme.MaruTheme
@@ -24,6 +28,27 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val TAG = "MyFirebaseMessagingService"
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(
+                    TAG,
+                    "Fetching FCM registration token failed",
+                    task.exception
+                )
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = token
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
+
         setContent {
             MaruTheme {
                 // A surface container using the 'background' color from the theme
--- front/app/src/main/java/com/shoebill/maru/ui/component/notice/NoticeContents.kt
+++ front/app/src/main/java/com/shoebill/maru/ui/component/notice/NoticeContents.kt
@@ -1,8 +1,6 @@
 package com.shoebill.maru.ui.component.notice
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,27 +14,39 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.shoebill.maru.R
 
 @Composable
 fun NoticeContents() {
-    var messages:ArrayList<String> = arrayListOf("11 경매에 낙찰되셨습니다.",
-        "더현대 방문 포인트를 받았습니다.", "서울현대박물관 경매에 낙찰되셨습니다.", "더현대 방문 포인트를 받았습니다.")
-    for( i in 1..50) {
+    // message 읽어오는 부분 필요
+    var messages: ArrayList<String> = arrayListOf(
+        "11 경매에 낙찰되셨습니다.",
+        "더현대 방문 포인트를 받았습니다.", "서울현대박물관 경매에 낙찰되셨습니다.", "더현대 방문 포인트를 받았습니다."
+    )
+
+    for (i in 1..50) {
         messages.add("서울현대박물관 경매에 낙찰되셨습니다.")
         messages.add("더현대 방문 포인트를 받았습니다.")
     }
+
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
     ) {
         items(messages) { message ->
-            NoticeListItem(message = message)
             Divider(
                 color = Color(0xFFE9E9E9),
-                thickness= 1.dp,
-                modifier = Modifier.padding(vertical = 15.dp))
+                thickness = 1.dp,
+                modifier = Modifier.padding(bottom = 15.dp)
+            )
+
+            NoticeListItem(message = message)
+
+            Divider(
+                color = Color(0xFFE9E9E9),
+                thickness = 1.dp,
+                modifier = Modifier.padding(top = 15.dp)
+            )
         }
     }
     NoticeListItem(message = "서울현대박물관 경매에 낙찰되셨습니다.")
@@ -47,8 +57,11 @@ fun NoticeListItem(message: String) {
     Row(verticalAlignment = Alignment.CenterVertically) {
         Image(
             painter = painterResource(R.drawable.notice_point_icon),
-            modifier = Modifier.padding(start = 20.dp, end = 10.dp).size(30.dp),
-            contentDescription = null)
+            modifier = Modifier
+                .padding(start = 20.dp, end = 10.dp)
+                .size(30.dp),
+            contentDescription = null
+        )
         Text(text = message)
     }
 }
