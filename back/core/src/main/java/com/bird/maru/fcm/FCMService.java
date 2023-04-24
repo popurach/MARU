e4de4891fb7d9ea1cdad04980269414456f318b4
ludwings0330 <ludwings0330@gmail.com>
2023-04-24 22:43:13 +0900

    :sparkles: Feat: FCM 메세지 전송 테스트
    - FCM 알림 전송 확인 완료

--- back/core/src/main/java/com/bird/maru/fcm/FCMService.java
+++ back/core/src/main/java/com/bird/maru/fcm/FCMService.java
@@ -2,24 +2,30 @@ package com.bird.maru.fcm;
 
 import com.google.firebase.messaging.AndroidConfig;
 import com.google.firebase.messaging.AndroidNotification;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FCMService {
 
-    public String sendMessage() {
-        Message.builder()
-               .setAndroidConfig(
-                       AndroidConfig.builder()
-                                    .setNotification(AndroidNotification.builder()
-                                                                        .setTitle("Test title")
-                                                                        .setBody("Test Body")
-                                                                        .build()).build()
-               )
-               .putData("test", "test")
-               .setToken("").build();
-        return null;
+    public String sendMessage() throws FirebaseMessagingException {
+        final Message message = Message.builder()
+                                       .setAndroidConfig(
+                                               AndroidConfig.builder()
+                                                            .setNotification(AndroidNotification.builder()
+                                                                                                .setTitle("Test title")
+                                                                                                .setBody("Test Body")
+                                                                                                .build()).build()
+                                       )
+                                       .putData("test", "test")
+                                       .setToken(
+                                               "dPdBxtyqTN-PFcrOuLQmqn:APA91bHIiatliaUqBgaJ75a46OEhozUI9HoXsgtDXyMTDoGJQhZ098Y12AItEb5zzBLE5EIr6tJJ9IwMOhIwlI8E2LqKQq48LirSdqbAkbk2PgtO0-NKtZIEORquN0R3iZu37MeOdTsG")
+                                       .build();
+
+        String response = FirebaseMessaging.getInstance().send(message);
+        return response;
     }
 
 }
