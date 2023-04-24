fcd4c54cac0b5d53b6edd24d75aa4f473229930c
DaWoon Lee <qkgk61@gmail.com>
2023-04-24 17:13:07 +0900

    :sparkles: Feat: JWT 관련 Filter 구현

--- back/core/src/main/java/com/bird/maru/common/filter/JwtAuthenticationFilter.java
+++ back/core/src/main/java/com/bird/maru/common/filter/JwtAuthenticationFilter.java
@@ -7,6 +7,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @RequiredArgsConstructor
@@ -15,9 +18,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String token = jwtUtil.resolveToken(request);
 
+        if (StringUtils.hasText(token) && jwtUtil.validate(token)) {
+            Authentication authentication = jwtUtil.createAuthentication(token);
+            SecurityContextHolder.getContext()
+                                 .setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
     }
 
 }
