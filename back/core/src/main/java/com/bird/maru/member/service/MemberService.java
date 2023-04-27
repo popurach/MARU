a18dad0664ca4a9f54ed10578a230915247a36d6
DaWoon Lee <qkgk61@gmail.com>
2023-04-27 18:08:03 +0900

    :sparkles: Feat: 회원 정보 수정 기능 구현
    - 이미지를 수정하지 않을 경우 image에 null
    - 닉네임을 수정하지 않을 경우 nickname에 null

---
+++ back/core/src/main/java/com/bird/maru/member/service/MemberService.java
@@ -0,0 +1,10 @@
+package com.bird.maru.member.service;
+
+import com.bird.maru.domain.model.entity.Member;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface MemberService {
+
+    Member modifyMemberInfo(Long memberId, String nickname, MultipartFile newImage);
+
+}
