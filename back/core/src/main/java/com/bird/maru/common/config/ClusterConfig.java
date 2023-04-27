315842e02338edc797e04fac2e85c1db74002f56
JunHyxxn <ihrug000@gmail.com>
2023-04-27 14:02:58 +0900

    :sparkles: Feat: DistanceMeasure 빈 설정

--- back/core/src/main/java/com/bird/maru/common/config/ClusterConfig.java
+++ back/core/src/main/java/com/bird/maru/common/config/ClusterConfig.java
@@ -1,8 +1,25 @@
 package com.bird.maru.common.config;
 
+import com.bird.maru.cluster.util.DistanceMeasure;
+import com.bird.maru.cluster.util.EuclideanDistance;
+import com.bird.maru.cluster.util.HaversineDistance;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ClusterConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "cluster.crs", havingValue = "MERCATOR", matchIfMissing = true)
+    public DistanceMeasure euclideanMeasure() {
+        return new EuclideanDistance();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "cluster.crs", havingValue = "WGS84")
+    public DistanceMeasure haversineMeasure() {
+        return new HaversineDistance();
+    }
 
 }
