f7406b277c880f8141c20169840c2a1c0f653aa1
JunHyxxn <ihrug000@gmail.com>
2023-04-28 16:26:44 +0900

    :hammer: Refactor: 자원 없을 시 Exception CustomException으로 수정(ResourceNotFoundException)

--- back/core/src/main/java/com/bird/maru/member/service/query/MemberQueryServiceImpl.java
+++ back/core/src/main/java/com/bird/maru/member/service/query/MemberQueryServiceImpl.java
@@ -1,8 +1,8 @@
 package com.bird.maru.member.service.query;
 
+import com.bird.maru.common.exception.ResourceNotFoundException;
 import com.bird.maru.domain.model.entity.Member;
 import com.bird.maru.member.repository.query.MemberQueryRepository;
-import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +17,7 @@ public class MemberQueryServiceImpl implements MemberQueryService {
     @Override
     public Member findMember(Long memberId) {
         return memberQueryRepository.findById(memberId)
-                                    .orElseThrow(() -> new NoSuchElementException("DB에서 데이터를 찾을 수 없습니다"));
+                                    .orElseThrow(() -> new ResourceNotFoundException("DB에서 데이터를 찾을 수 없습니다"));
     }
 
 }
