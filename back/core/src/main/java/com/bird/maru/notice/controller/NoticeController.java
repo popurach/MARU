9b73561fccb9a6758d01183f01895223d65d7c04
ludwings0330 <ludwings0330@gmail.com>
2023-04-28 15:53:44 +0900

    :sparkles: Feat: Back-end Notice Controller 수정
    - 반환값을 Front-end 에 맞춤

--- back/core/src/main/java/com/bird/maru/notice/controller/NoticeController.java
+++ back/core/src/main/java/com/bird/maru/notice/controller/NoticeController.java
@@ -1,12 +1,17 @@
 package com.bird.maru.notice.controller;
 
+import com.bird.maru.domain.model.type.CustomUserDetails;
+import com.bird.maru.notice.controller.dto.NoticeResponseDto;
 import com.bird.maru.notice.model.Notice;
 import com.bird.maru.notice.service.NoticeService;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +25,10 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     @GetMapping
-    public Slice<Notice> findNotice(@PageableDefault(size = 15) Pageable pageable) {
-        return noticeService.findByMemberId(1L, pageable);
+    public List<NoticeResponseDto> findNotice(@PageableDefault(size = 15) Pageable pageable, @AuthenticationPrincipal CustomUserDetails member) {
+        final Slice<Notice> notices = noticeService.findByMemberId(1L, pageable);
+
+        return notices.stream().map(NoticeResponseDto::new).collect(Collectors.toList());
     }
 
 }
