c32713b7ac825451467ca28a606bc781e2c6e182
ludwings0330 <ludwings0330@gmail.com>
2023-04-28 14:17:19 +0900

    :sparkles: Feat: NoticeToken 저장 구현
    - 로그인시 NoticeToken 저장되도록 구현

--- front/app/src/main/java/com/shoebill/maru/MainActivity.kt
+++ front/app/src/main/java/com/shoebill/maru/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.shoebill.maru
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -24,8 +22,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.shoebill.maru.ui.page.LoginPage
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
 import com.shoebill.maru.ui.page.MainPage
 import com.shoebill.maru.ui.page.NoticePage
 import com.shoebill.maru.ui.theme.MaruTheme
@@ -45,26 +41,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        val TAG = "MyFirebaseMessagingService"
-
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                Log.w(
-                    TAG,
-                    "Fetching FCM registration token failed",
-                    task.exception
-                )
-                return@OnCompleteListener
-            }
-
-            // Get new FCM registration token
-            val token = task.result
-
-            // Log and toast
-            val msg = token
-            Log.d(TAG, msg)
-            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-        })
 
         setContent {
             MaruTheme {
--- front/app/src/main/java/com/shoebill/maru/viewmodel/MemberViewModel.kt
+++ front/app/src/main/java/com/shoebill/maru/viewmodel/MemberViewModel.kt
@@ -4,11 +4,15 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.shoebill.maru.model.data.Member
 import com.shoebill.maru.model.repository.MemberRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,12 +26,40 @@ class MemberViewModel @Inject constructor(
         _memberInfo.value = value
     }
 
+    private fun updateNoticeToken() {
+        val TAG = "MyFirebaseMessagingService"
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(
+                    TAG,
+                    "Fetching FCM registration token failed",
+                    task.exception
+                )
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            runBlocking {
+                launch {
+                    memberRepository.updateNoticeToken(
+                        token.toRequestBody("text/plain".toMediaType())
+                    )
+                }
+            }
+        })
+    }
+
     fun getMemberInfo() {
         runBlocking {
             launch {
                 val response = memberRepository.getMemberInfo()
                 if (response.isSuccessful) {
                     updateMemberInfo(response.body()!!)
+                    // notice token update 필요
+                    updateNoticeToken()
                 } else {
                     Log.d("MEMBER", "회원 정보 조회 실패")
                 }
