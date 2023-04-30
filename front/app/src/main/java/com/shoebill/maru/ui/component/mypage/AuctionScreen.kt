0b4d6ed8f7cb6022f507f88aa445e1642abfa129
ludwings0330 <ludwings0330@gmail.com>
2023-04-30 19:12:28 +0900

    :sparkles: Feat: mypage Tab component 생성

---
+++ front/app/src/main/java/com/shoebill/maru/ui/component/mypage/AuctionScreen.kt
@@ -0,0 +1,17 @@
+package com.shoebill.maru.ui.component.mypage
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun AuctionScreen() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Text(text = "AuctionScreen")
+    }
+}
+
+
