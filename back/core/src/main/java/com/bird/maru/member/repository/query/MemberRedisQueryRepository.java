987a966f92d5a0da3195917127d4ed1b2b0ffa96
JunHyxxn <ihrug000@gmail.com>
2023-05-01 23:31:21 +0900

    :sparkles: Feat: 랜드마크 방문 API 구현
    ### 랜드마크 방문 로직
    - Redis에서 방문한 랜드마크 조회
    - 최초 방문 시 다가오는 자정에 redis 삭제되도록 만료 시간 설정한 후 Redis에 저장
    - 이미 방문했다면 추가 로직 수행 없이 얻은 포인트 0으로 응답
    - 최초 방문 시 포인트 전략에 따라 추가 후 얼마 획득했는지 반환
    - 초기 버전 :  고정 값 반환

--- back/core/src/main/java/com/bird/maru/member/repository/query/MemberRedisQueryRepository.java
+++ back/core/src/main/java/com/bird/maru/member/repository/query/MemberRedisQueryRepository.java
@@ -1,12 +1,17 @@
 package com.bird.maru.member.repository.query;
 
 import com.bird.maru.common.redis.RedisCacheKey;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
@@ -22,6 +27,16 @@ public class MemberRedisQueryRepository {
         return members == null || members.isEmpty() ? new HashSet<>()
                 : members.stream().map(m -> Long.parseLong(m.toString()))
                          .collect(Collectors.toSet());
+    }
+
+    public Long insertVisitLandmark(Long memberId, Long landmarkId) {
+        SetOperations<String, Object> ops = redisTemplate.opsForSet();
+        LocalDateTime midnight = LocalDate.now().plusDays(1).atTime(LocalTime.MIDNIGHT);
+        long expirationInSecs = midnight.toEpochSecond(ZoneOffset.UTC) - LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+        String key = RedisCacheKey.createKey(RedisCacheKey.MEMBER_VISITED, memberId);
+        Long result = ops.add(key, landmarkId.toString());
+        redisTemplate.expire(key, expirationInSecs, TimeUnit.SECONDS);
+        return result;
     }
 
 }
--- back/core/src/main/java/com/bird/maru/member/service/MemberService.java
+++ back/core/src/main/java/com/bird/maru/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.bird.maru.member.service;
 
+import com.bird.maru.common.exception.ResourceNotFoundException;
 import com.bird.maru.domain.model.entity.Member;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,5 +9,7 @@ public interface MemberService {
     Member modifyMemberInfo(Long memberId, String nickname, MultipartFile newImage);
 
     void registerNoticeToken(Long memberId, String noticeToken);
+
+    Integer gainPoint(Long memberId, Long landmarkId) throws ResourceNotFoundException;
 
 }
