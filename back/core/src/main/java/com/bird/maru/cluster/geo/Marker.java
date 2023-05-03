baca2936c8204655f300a0f078560f67f9f7a868
JunHyxxn <ihrug000@gmail.com>
2023-05-03 14:57:30 +0900

    :sparkles: Feat: 지도 기반 스팟 좌표 조회 Repository - RDBMS 구현
    - Projections.fields는 NoArgsConstructor + Reflection 방식
    - Marker의 패키지가 repository랑 다르기 때문에 @NoArgsConstructor의 접근 제한자를 해제했습니다.

--- back/core/src/main/java/com/bird/maru/cluster/geo/Marker.java
+++ back/core/src/main/java/com/bird/maru/cluster/geo/Marker.java
@@ -1,7 +1,6 @@
 package com.bird.maru.cluster.geo;
 
 import com.bird.maru.domain.model.type.Coordinate;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,8 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @ToString
 public class Marker {
