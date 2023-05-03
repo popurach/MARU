382ae4af43a0d20727dd408512c8c4056f006061
DaWoon Lee <qkgk61@gmail.com>
2023-05-03 15:14:08 +0900

    :sparkles: Feat: 동시성 문제가 발생하지 않도록 좋아요 토글 로직 수정

--- back/core/src/main/java/com/bird/maru/spot/controller/SpotController.java
+++ back/core/src/main/java/com/bird/maru/spot/controller/SpotController.java
@@ -1,6 +1,7 @@
 package com.bird.maru.spot.controller;
 
 import com.bird.maru.auth.service.dto.CustomUserDetails;
+import com.bird.maru.common.util.NamedLockExecutor;
 import com.bird.maru.like.service.LikeService;
 import com.bird.maru.spot.controller.dto.SpotSearchCondition;
 import com.bird.maru.spot.repository.query.dto.SpotSimpleDto;
@@ -22,6 +23,7 @@ public class SpotController {
 
     private final SpotQueryService spotQueryService;
     private final LikeService likeService;
+    private final NamedLockExecutor namedLockExecutor;
 
     /**
      * 내 스팟 목록 조회에 성공할 경우 스팟 목록과 상태 코드 200을 반환합니다.
@@ -55,12 +57,23 @@ public class SpotController {
         return spotQueryService.findMyScraps(member.getId(), condition);
     }
 
+    /**
+     * 좋아요 개수를 높이는 것은 동시성 문제가 발생할 수 있습니다.
+     * 따라서 Named Lock을 이용하여 좋아요 여부를 토글하도록 구현했습니다.
+     *
+     * @param member 현재 로그인 한 회원
+     * @param spotId 회원이 좋아요를 토글하려는 스팟
+     */
     @PostMapping("/{spotId}/like")
     public void toggleLike(
             @AuthenticationPrincipal CustomUserDetails member,
             @PathVariable Long spotId
     ) {
-        likeService.toggleLike(member.getId(), spotId);
+        namedLockExecutor.executeWithLock(
+                spotId.toString(),
+                5,
+                () -> likeService.toggleLike(member.getId(), spotId)
+        );
     }
 
 }
