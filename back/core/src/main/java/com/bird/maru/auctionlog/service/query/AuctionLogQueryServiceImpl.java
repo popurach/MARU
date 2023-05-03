c3802a2659f211300d905d1a1c1947910bec8f1f
DaWoon Lee <qkgk61@gmail.com>
2023-05-04 00:39:35 +0900

    :poop: Fix: Transactional 애노테이션 추가

--- back/core/src/main/java/com/bird/maru/auctionlog/service/query/AuctionLogQueryServiceImpl.java
+++ back/core/src/main/java/com/bird/maru/auctionlog/service/query/AuctionLogQueryServiceImpl.java
@@ -6,8 +6,10 @@ import com.bird.maru.domain.model.entity.AuctionLog;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AuctionLogQueryServiceImpl implements AuctionLogQueryService {
 
