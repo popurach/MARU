31af8513cc016a48f54768de5a2708c5e40f2863
JunHyxxn <ihrug000@gmail.com>
2023-05-03 16:11:21 +0900

    :sparkles: Feat: 지도 내 스팟 조회 - 클러스터링 적용 구현

--- back/core/src/main/java/com/bird/maru/map/controller/MapController.java
+++ back/core/src/main/java/com/bird/maru/map/controller/MapController.java
@@ -3,20 +3,29 @@ package com.bird.maru.map.controller;
 import com.bird.maru.auth.service.dto.CustomUserDetails;
 import com.bird.maru.landmark.controller.dto.LandmarkMapResponseDto;
 import com.bird.maru.landmark.service.query.LandmarkQueryService;
+import com.bird.maru.map.controller.dto.MapCondition;
+import com.bird.maru.map.service.query.MapQueryService;
+import com.bird.maru.spot.service.query.SpotQueryService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.wololo.geojson.Feature;
 
 @RestController
 @RequestMapping("/api/map")
 @RequiredArgsConstructor
+@Slf4j
 public class MapController {
 
     private final LandmarkQueryService landmarkQueryService;
+    private final MapQueryService mapQueryService;
 
     /**
      * 랜드마크 지도 기준 검색 - 랜드마크는 클러스터링 진행하지 않습니다. <br/> 랜드마크 정보에는 해당 사용자의 최초 방문 여부를 함께 반환합니다. 결과가 존재하지 않다면 빈 리스트를 반환하고, <br/> 결과가 존재한다면 좌표 정보와 함께 사용자가
@@ -38,5 +47,19 @@ public class MapController {
         return landmarkQueryService.findLandmarkBasedMap(west, south, east, north, member.getId());
     }
 
+    /**
+     * 지도 기반 스팟 목록 조회 - 클러스터 적용 <br/>
+     * 단일 좌표 - properties.geoType = Point <br/>
+     * 클러스터 대표 좌표 - properties.geoype = Cluster <br/>
+     * 
+     * @param member : 현재 접근중인 주체
+     * @param condition : 검색 조건
+     * @return 데이터 없을 시 빈 배열 반환
+     * */
+    @PostMapping("/spots")
+    public List<Feature> spotsCluster(@AuthenticationPrincipal CustomUserDetails member, @RequestBody MapCondition condition) {
+        log.debug("{}", condition);
+        return mapQueryService.spotsCluster(member.getId(), condition);
+    }
 
 }
