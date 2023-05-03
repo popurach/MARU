b8cabd105abe787de06013774eb0cf0a631aa7a3
JunHyxxn <ihrug000@gmail.com>
2023-05-03 10:14:41 +0900

    :poop: Fix: 삭제된 코드 복구

--- back/core/src/main/java/com/bird/maru/member/repository/query/MemberRedisRepository.java
+++ back/core/src/main/java/com/bird/maru/member/repository/query/MemberRedisRepository.java
@@ -4,7 +4,10 @@ import com.bird.maru.common.redis.RedisCacheKey;
 import com.bird.maru.common.util.TimeUtil;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
@@ -15,6 +18,15 @@ import org.springframework.stereotype.Repository;
 public class MemberRedisRepository {
 
     private final RedisTemplate<String, Object> redisTemplate;
+
+    public Set<Long> findVisitedLandmarks(Long memberId) {
+        Set<Object> members = redisTemplate.opsForSet().members(
+                RedisCacheKey.MEMBER_VISITED.getKey(memberId)
+        );
+        return members == null || members.isEmpty() ? new HashSet<>()
+                : members.stream().map(m -> Long.parseLong(m.toString()))
+                         .collect(Collectors.toSet());
+    }
 
     public Boolean existVisitedLandmark(Long memberId, Long landmarkId) {
         return redisTemplate.opsForSet().isMember(
