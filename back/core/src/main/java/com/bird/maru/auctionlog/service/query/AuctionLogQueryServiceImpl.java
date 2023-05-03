7723c76d499f49cb324f3c367bef7efe7959b72d
DaWoon Lee <qkgk61@gmail.com>
2023-05-03 18:04:22 +0900

    :sparkles: Feat: 내가 참여한 경매 목록 조회

---
+++ back/core/src/main/java/com/bird/maru/auctionlog/service/query/AuctionLogQueryServiceImpl.java
@@ -0,0 +1,21 @@
+package com.bird.maru.auctionlog.service.query;
+
+import com.bird.maru.auctionlog.controller.dto.AuctionLogSearchCondition;
+import com.bird.maru.auctionlog.repository.query.AuctionLogCustomQueryRepository;
+import com.bird.maru.domain.model.entity.AuctionLog;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuctionLogQueryServiceImpl implements AuctionLogQueryService {
+
+    private final AuctionLogCustomQueryRepository auctionLogCustomQueryRepository;
+
+    @Override
+    public List<AuctionLog> findMyAuctions(Long memberId, AuctionLogSearchCondition condition) {
+        return auctionLogCustomQueryRepository.findAllWithAuctionByMemberAndCondition(memberId, condition);
+    }
+
+}
