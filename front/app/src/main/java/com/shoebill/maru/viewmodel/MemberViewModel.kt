784f96fb05d017153cfbd14968eb4e5d7ec50fbf
ludwings0330 <ludwings0330@gmail.com>
2023-05-04 16:13:38 +0900

    :sparkles: Feat: 프로필 닉네임 수정 완료
    - 이미지 수정은 하는 중

--- front/app/src/main/java/com/shoebill/maru/viewmodel/MemberViewModel.kt
+++ front/app/src/main/java/com/shoebill/maru/viewmodel/MemberViewModel.kt
@@ -1,12 +1,17 @@
 package com.shoebill.maru.viewmodel
 
+import android.content.Context
+import android.net.Uri
+import android.provider.MediaStore
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 import com.shoebill.maru.model.data.Member
+import com.shoebill.maru.model.data.MemberUpdateRequest
 import com.shoebill.maru.model.repository.MemberRepository
 import com.shoebill.maru.util.PreferenceUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,6 +19,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
 import java.text.NumberFormat
 import java.util.Locale
 import javax.inject.Inject
@@ -24,6 +30,9 @@ class MemberViewModel @Inject constructor(
     private val preferenceUtil: PreferenceUtil,
 ) : ViewModel() {
     private val _memberInfo = MutableLiveData<Member>()
+    val modifiedNickname = MutableLiveData<String?>()
+    val modifiedImageUri = MutableLiveData<Uri?>()
+
     val memberInfo: LiveData<Member> get() = _memberInfo
 
     fun updateMemberInfo(value: Member) {
@@ -80,5 +89,42 @@ class MemberViewModel @Inject constructor(
 
     fun logout() {
         memberRepository.logout()
+    }
+
+    fun updateMemberProfileToServer(context: Context) {
+        val imageUri = modifiedImageUri.value
+        val nickname = modifiedNickname.value
+        var file: File? = null
+        if (imageUri != null) {
+            file = uriToFile(context, imageUri)
+        }
+        viewModelScope.launch {
+            val response = memberRepository.updateMemberInfo(
+                MemberUpdateRequest(
+                    image = file,
+                    nickname = nickname
+                )
+            )
+
+            if (response.isSuccessful) {
+                response.body()?.let { updateMemberInfo(it) }
+            }
+        }
+    }
+
+    private fun uriToFile(context: Context, uri: Uri): File {
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+
+        val cursor = context.contentResolver.query(uri, projection, null, null, null)
+        cursor?.let {
+            val columnIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            it.moveToFirst()
+            val filePath = it.getString(columnIndex)
+            
+            cursor.close()
+            return File(filePath)
+        }
+
+        throw IllegalArgumentException("Uri not found")
     }
 }
