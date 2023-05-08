23ef8995f0b671bf86fbf57c11a0485632bfeb17
DESKTOP-DOGVPUB\SSAFY <kares64@gmail.com>
2023-05-08 14:17:12 +0900

    :sparkles: Feat: 지도 클러스터 및 클릭 이벤트 추가

--- front/app/src/main/java/com/shoebill/maru/util/SpotType.kt
+++ front/app/src/main/java/com/shoebill/maru/util/SpotType.kt
@@ -3,5 +3,13 @@ package com.shoebill.maru.util
 object SpotType {
     const val LANDMARK = 0
     const val SPOT = 1
-    const val CLUSTER = 2
+    const val CLUSTER_2 = 2
+    const val CLUSTER_3 = 3
+    const val CLUSTER_4 = 4
+    const val CLUSTER_5 = 5
+    const val CLUSTER_6 = 6
+    const val CLUSTER_7 = 7
+    const val CLUSTER_8 = 8
+    const val CLUSTER_9 = 9
+    const val CLUSTER_MORE_9 = 10
 }
