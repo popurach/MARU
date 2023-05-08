bdcf67911f64a2bc1cefde71540590d4af663d8f
JunHyxxn <ihrug000@gmail.com>
2023-05-08 17:40:53 +0900

    :hammer: Refactor: 랜드마크 소유자 정보 조회 리팩토링

--- back/core/src/main/java/com/bird/maru/landmark/service/query/LandmarkQueryService.java
+++ back/core/src/main/java/com/bird/maru/landmark/service/query/LandmarkQueryService.java
@@ -4,6 +4,7 @@ import com.bird.maru.common.exception.ResourceNotFoundException;
 import com.bird.maru.domain.model.entity.Landmark;
 import com.bird.maru.landmark.controller.dto.LandmarkMapResponseDto;
 import com.bird.maru.landmark.controller.dto.LandmarkStampResponseDto;
+import com.bird.maru.landmark.controller.dto.OwnerResponseDto;
 import java.util.List;
 
 public interface LandmarkQueryService {
@@ -13,5 +14,7 @@ public interface LandmarkQueryService {
     List<LandmarkMapResponseDto> findLandmarkBasedMap(Double west, Double south, Double east, Double north, Long memberId);
 
     List<LandmarkStampResponseDto> findLandmarkStamps(Long memberId, Long lastOffset, Integer size);
+
+    OwnerResponseDto findOwnerData(Long landmarkId) throws ResourceNotFoundException;
 
 }
