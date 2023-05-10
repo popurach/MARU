643b8a0dec8df1ad295f4678a0296684a7d9dcd2
DESKTOP-DOGVPUB\SSAFY <kares64@gmail.com>
2023-05-10 09:08:24 +0900

    :hammer: Refactor: 페이지네이션 소스파일 디렉토리 분리

--- front/app/src/main/java/com/shoebill/maru/model/source/NoticeSource.kt
+++ front/app/src/main/java/com/shoebill/maru/model/source/NoticeSource.kt
@@ -1,8 +1,9 @@
-package com.shoebill.maru.model.repository
+package com.shoebill.maru.model.source
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.shoebill.maru.model.data.notice.Notice
+import com.shoebill.maru.model.repository.NoticeRepository
 
 class NoticeSource(private val noticeRepository: NoticeRepository) : PagingSource<Int, Notice>() {
     override fun getRefreshKey(state: PagingState<Int, Notice>): Int? {
