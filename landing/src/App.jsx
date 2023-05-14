d7aa3a788c52a4002e1caf179ce84bf0655bd764
yejin <yejinlee0707@gmail.com>
2023-05-15 00:46:24 +0900

    :tada: Init: landing init

---
+++ landing/src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Sbackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(118.85deg, #6039df 8.23%, #a14ab7 91.34%);
+`;
+
+function App() {
+  return <Sbackground></Sbackground>;
+}
+
+export default App;
