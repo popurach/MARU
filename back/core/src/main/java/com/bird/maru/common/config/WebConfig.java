d1bc7cc0ad3b3f878d81ed5cd2287bfc3ebe2bd5
DaWoon Lee <qkgk61@gmail.com>
2023-05-15 15:04:17 +0900

    :bug: Fix: ResponseEntity를 활용하여 응답 반환하도록 정의

--- back/core/src/main/java/com/bird/maru/common/config/WebConfig.java
+++ back/core/src/main/java/com/bird/maru/common/config/WebConfig.java
@@ -1,14 +1,8 @@
 package com.bird.maru.common.config;
 
 import com.bird.maru.common.config.converter.StringToEnumConverterFactory;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
--- back/core/src/main/java/com/bird/maru/common/handler/GlobalExceptionHandler.java
+++ back/core/src/main/java/com/bird/maru/common/handler/GlobalExceptionHandler.java
@@ -5,7 +5,10 @@ import com.bird.maru.common.exception.ResourceConflictException;
 import com.bird.maru.common.exception.ResourceNotFoundException;
 import javax.naming.AuthenticationException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,46 +19,53 @@ import org.springframework.web.client.HttpClientErrorException.UnprocessableEnti
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final HttpHeaders headers;
+
+    static {
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
     @ExceptionHandler({ IllegalArgumentException.class, NotEnoughMoneyException.class })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleBadRequest(Exception e) {
-        return e.getMessage();
+    public ResponseEntity<String> handleBadRequest(Exception e) {
+        return responseError(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler({ AuthenticationException.class, JwtException.class })
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public String handleUnAuthentication(Exception e) {
-        return e.getMessage();
+    public ResponseEntity<String> handleUnAuthentication(Exception e) {
+        return responseError(e.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler({ AccessDeniedException.class })
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public String handleAuthorization(Exception e) {
-        return e.getMessage();
+    public ResponseEntity<String> handleAuthorization(Exception e) {
+        return responseError(e.getMessage(), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler({ ResourceNotFoundException.class })
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNotFound(Exception e) {
-        return e.getMessage();
+    public ResponseEntity<String> handleNotFound(Exception e) {
+        return responseError(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({ ResourceConflictException.class, DataIntegrityViolationException.class })
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public String handleConflict(Exception e) {
-        return e.getMessage();
+    public ResponseEntity<String> handleConflict(Exception e) {
+        return responseError(e.getMessage(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler({ UnprocessableEntity.class })
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public String handleUnprocessableParams(Exception e) {
-        return e.getMessage();
+    public ResponseEntity<String> handleUnprocessableParams(Exception e) {
+        return responseError(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     @ExceptionHandler({ RuntimeException.class })
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleRuntimeException(Exception e) {
-        return e.getMessage();
+    public ResponseEntity<String> handleRuntimeException(Exception e) {
+        return responseError(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<String> responseError(String message, HttpStatus status) {
+        return new ResponseEntity<>(message, headers, status);
     }
 
 }
