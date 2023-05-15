9aba7638b42a5b42f549dac0a40a37e4c1802d20
DaWoon Lee <qkgk61@gmail.com>
2023-05-15 12:59:32 +0900

    :sparkles: Feat: Spot 저장 시 태그를 ElasticSearch 서버에도 저장하도록 수정

--- back/core/src/main/java/com/bird/maru/domain/model/document/TagDoc.java
+++ back/core/src/main/java/com/bird/maru/domain/model/document/TagDoc.java
@@ -1,15 +1,13 @@
 package com.bird.maru.domain.model.document;
 
+import com.bird.maru.domain.model.entity.Tag;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 import org.springframework.data.elasticsearch.annotations.Setting;
 
 @Document(indexName = "tags")
@@ -19,13 +17,15 @@ import org.springframework.data.elasticsearch.annotations.Setting;
 @Getter
 @ToString
 @EqualsAndHashCode
-public class Tag {
+public class TagDoc {
 
-    @Id
-    @Field(type = FieldType.Long)
     private Long id;
 
-    @Field(type = FieldType.Text)
     private String name;
+
+    public TagDoc(Tag tag) {
+        this.id = tag.getId();
+        this.name = tag.getName();
+    }
 
 }
