b1589c2fd2fe5b0770f4774824772bacbdf5afc5
DESKTOP-DOGVPUB\SSAFY <kares64@gmail.com>
2023-05-17 18:03:23 +0900

    :hammer: Refactor: 로딩 인디케이터 텍스트 제거

--- front/app/src/main/java/com/shoebill/maru/ui/component/common/CustomCircularProgressBar.kt
+++ front/app/src/main/java/com/shoebill/maru/ui/component/common/CustomCircularProgressBar.kt
@@ -3,14 +3,12 @@ package com.shoebill.maru.ui.component.common
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CustomCircularProgressBar(
-    text: String
 ) {
     CustomAlertDialog(onDismissRequest = { }) {
         Column {
@@ -18,7 +16,6 @@ fun CustomCircularProgressBar(
                 modifier = Modifier.size(100.dp),
                 strokeWidth = 10.dp
             )
-            Text(text = text)
         }
     }
 }
